288eea070a74e046a3636653c683db66a45419da
HoangKhang <nguyenhoangkhq7@gmail.com>
2026-04-26 16:49:50 +0700

    feat: initialize identity-service and implement UserMapper with MapStruct

--- services/identity-service/src/main/java/fit/iuh/mapper/UserMapper.java
+++ services/identity-service/src/main/java/fit/iuh/mapper/UserMapper.java
@@ -4,16 +4,19 @@ import fit.iuh.dto.request.RegisterUserRequest;
 import fit.iuh.dto.request.UpdateUserRequest;
 import fit.iuh.dto.response.UserDto;
 import fit.iuh.entity.User;
-import org.mapstruct.Builder;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 
-@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
+@Mapper(
+    componentModel = "spring",
+    builder = @Builder(disableBuilder = true),
+    unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface UserMapper {
    UserDto toDto(User user);
 
    @Mapping(source = "name", target = "fullName")
    User toEntity(RegisterUserRequest request);
+
+   @Mapping(source = "name", target = "fullName")
    void updateUser(UpdateUserRequest request, @MappingTarget User user);
 }
