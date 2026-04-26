87510d68c7a9dd0458dc1d2806057e43a41cdf3c
HoangKhang <nguyenhoangkhq7@gmail.com>
2026-04-26 17:09:17 +0700

    feat: implement scheduled token cleanup service in identity-service

--- services/identity-service/src/main/java/fit/iuh/IdentityServiceApplication.java
+++ services/identity-service/src/main/java/fit/iuh/IdentityServiceApplication.java
@@ -1,12 +1,13 @@
 package fit.iuh;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableScheduling
 public class IdentityServiceApplication {
 
    public static void main(String[] args) {
